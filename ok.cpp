2acbb16327def7d664cc1a516e6eacc54b09553b
thuong56 <elitaejoeo008g@hotmail.com>
2025-02-25 16:04:58 +0800

    Modify ok.cpp

--- ok.cpp
+++ ok.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Design foot manage age society pressure approach by.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
